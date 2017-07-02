bbe6ef1eba96d14f07f1801e70d6dc6e3faff916
tueseed <tue_seed@hotmail.com>
2017-07-02 21:44:04 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Upload your files</title>
+  <title>อัพโหลดไฟล์ฐานข้อมูลสำหรับ PEA SMART LOCATION</title>
 </head>
 <body>
   <form enctype="multipart/form-data" action="upload.php" method="POST">
-    <p>Upload your file</p>
+    <p>อัพโหลดไฟล์ฐานข้อมูลสำหรับ PEA SMART LOCATION</p>
     <input type="file" name="uploaded_file"></input><br />
     <input type="submit" value="Upload"></input>
   </form>
@@ -14,7 +14,7 @@
 <?PHP
   if(!empty($_FILES['uploaded_file']))
   {
-    $path = "uploads/";
+    $path = "pea-smart-location/";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
       echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
